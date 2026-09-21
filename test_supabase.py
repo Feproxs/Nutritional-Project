5adf0e39e97b52115742a5769faba32d2ff8dc1d
Marcos Portillo <marcosportillo@MacBook-Pro-de-Marcos-3.local>
2026-09-21 19:57:54 +0200

    Conexión con Supabase funcionando

---
+++ test_supabase.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+from supabase import create_client
+import os
+
+load_dotenv()
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(url, key)
+
+print("Conexión creada correctamente ✅")
